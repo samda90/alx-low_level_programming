2ac908ae3e6725b835eca1a2a842960f46f818e6
samda90 <samd902016@gmail.com>
2022-06-05 10:28:13 -0700

    variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Program to print single digit numbers, 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
