23b0f73bb2a09e1533a02009656ceb4fb51ac976
samda90 <samd902016@gmail.com>
2022-06-10 16:11:22 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - function that prints the numbers, from 0 to 9, followed by a new line
+ *
+ *Return: Nill 
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
