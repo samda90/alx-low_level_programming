775e089c87275ebe6133d9209416117f6cd3c549
samda90 <samd902016@gmail.com>
2022-06-10 15:56:19 -0700

    nested

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Function that multiplies two integers
+ *
+ *Return: Always results of multiplication (Success)
+ */
+
+	int mul(int a, int b)
+{
+	 int _mult;
+	 _mult = (a * b);
+	 return (_mult);
+}
