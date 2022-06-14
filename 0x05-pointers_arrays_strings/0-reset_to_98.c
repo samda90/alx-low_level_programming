bd757a689e80a388d9a0ea0d7ccd057358b71651
samda90 <samd902016@gmail.com>
2022-06-14 07:41:46 -0700

    pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - update pointer value to 98
  *
  * Return: Always 0.
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
-
 }
