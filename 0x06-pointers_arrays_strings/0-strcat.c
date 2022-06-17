5a6331b14b208ffb9c6833384f80732be070f79f
samda90 <samd902016@gmail.com>
2022-06-17 15:32:40 -0700

    more pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,27 @@
+#include <string.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
- * strcat - function that concatenates two strings
- * @s1 s2: pointer variable
+ * _strcat - concatenates two strings,
  *
- * dest - is the destination where the source
- * src - is the the string to me moved
- * Return: Concatenation of two strings
- * Main - Void
- * _strcat - used to concatenate strings
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: the pointer to dest.
  */
+
 char *_strcat(char *dest, char *src)
-
 {
-int main(void)
-{
-	char ch1[50] = "";
-	char ch2[50] = "";
+	int index, destlen = strlen(dest), srclen = strlen(src);
 
-	printf("\n 1st String : [ %s]", ch1);
-	printf("\n\n 2nd String : [ %s ]", ch2);
-	strcat(ch1, ch2);
-	printf("\n\n Concatenated String is : [ %s ]\n", ch1);
-	return (0);
+	for (index = 0; index < srclen; index++)
+	{
+		dest[destlen + index] = src[index];
+	}
+	if (1)
+	{
+		dest[destlen + index] = '\0';
+	}
+	return (dest);
 }
-
