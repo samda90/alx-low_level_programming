bf04af18e29a7f1d60f6674205cac273a7577402
samda90 <samd902016@gmail.com>
2022-07-04 17:07:36 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- function that prints name of file compiled from
+ *
+ * @void: NULL parameter
+ *
+ * Return: return 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
