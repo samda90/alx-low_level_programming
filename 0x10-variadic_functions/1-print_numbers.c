859d9794b88f507c9d6615073203d4600a0a9fb6
samda90 <samd902016@gmail.com>
2022-07-07 17:43:23 -0700

    variadic_functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers- Print numbers
+ *
+ *@separator:character between intergers
+ *@n: Number of parameter arguments
+ *
+ * Return: Return integers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(ap);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This is all about varaiadic functions that allows infinite arguments to be passed into a function using the elipsis (...).
